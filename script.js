948c61d3f528f2b185926fa09332f62cf65d73f9
isakreddy02 <20551a4208.isakreddy@gmail.com>
2023-07-01 15:47:01 +0530

    Oyasis Task-2

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add smooth scrolling to nav links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
